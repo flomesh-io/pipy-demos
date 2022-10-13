5b52599f3db43032683ddbf05980c351f52ed5e2
Addo.Zhang <duwasai@gmail.com>
2022-10-13 17:30:06 +0800

    add script

---
+++ thrift-demo/pipy/plugins/throttle.js
@@ -0,0 +1,36 @@
+((
+  config = pipy.solve('config.js'),
+) => pipy({
+  quotas: Object.fromEntries(
+    Object.entries(config.rateLimit).map(
+      ([svc, limit]) => [
+        svc, limit && limit.rate > 0 ?
+          new algo.Quota(limit.rate, { produce: limit.rate, per: limit.window ? limit.window : '1s' })
+          :
+          null
+      ]
+    )
+  ),
+  _quota: undefined,
+})
+  .import({
+    __service: 'main',
+  })
+
+
+  .pipeline()
+  .handleStreamStart(
+    () => _quota = quotas[__service]
+  )
+  .branch(
+    () => Boolean(_quota), ($ => $
+      .muxQueue({}, { maxQueue: 0 }).to($ => $
+        .throttleMessageRate(
+          () => _quota
+        )
+        .demuxQueue().to($ => $.chain())
+      )
+  ),
+    $ => $.chain()
+  )
+)()
