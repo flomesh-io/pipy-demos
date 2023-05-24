d7c00e13f2227cdc2c7c9f0c4527c144174ccc43
leopeng1995 <leopeng1995@hotmail.com>
2023-05-25 00:38:58 +0800

    refactor(pipy-bgp-speaker): refactor pipy-bgp-speaker sample

---
+++ pipy-bgp-speaker/main.js
@@ -0,0 +1,44 @@
+((
+  config = JSON.decode(pipy.load('config.json')),
+  newPeer = pipy.solve('peer.js'),
+  allPeers = config.peers.map(addr => newPeer(config, addr))
+
+) => pipy({
+  _peer: null,
+})
+
+.task('1s')
+.onStart(() => [new Message, new StreamEnd])
+.fork(allPeers).to(
+  $=>$
+  .onStart(peer => void (_peer = peer))
+  .replaceMessage(() => _peer.tick())
+  .replaceStreamEnd()
+  .demux().to(
+    $=>$.mux(() => _peer).to(
+      $=>$
+      .encodeBGP({ enableAS4: () => _peer.isAS4() })
+      .handleMessage(msg => console.debug('>>>', _peer.state(), msg.payload))
+      .connect(() => _peer.destination, { idleTimeout: 0 })
+      .decodeBGP({ enableAS4: () => _peer.isAS4() })
+      .handleMessage(msg => console.debug('<<<', _peer.state(), msg.payload))
+      .handleMessage(msg => _peer.receive(msg))
+      .handleStreamEnd(() => _peer.end())
+    )
+  )
+)
+
+.watch('config.json')
+.onStart(() => (
+  (
+    update = JSON.decode(pipy.load('config.json')),
+  ) => (
+    console.info('config.json updated to', update),
+    config.ipv4 = update.ipv4,
+    config.ipv6 = update.ipv6,
+    allPeers.forEach(peer => peer.update()),
+    new StreamEnd
+  )
+)())
+
+)()
